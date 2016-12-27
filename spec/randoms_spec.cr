1100ed6d4aee21710cbd94d191d7c1a1f48d9575
Chase Southard <chase.southard@gmail.com>
2016-12-27 15:59:37 -0500

    add tests.
    man, that took way too long to realize that .to_s was the key to
    making it happen

--- spec/randoms_spec.cr
+++ spec/randoms_spec.cr
@@ -1,9 +1,38 @@
 require "./spec_helper"
 
 describe Randoms do
-  # TODO: Write tests
+  describe "GET /ping" do
+    it "renders pong" do
+      get "/ping"
+      response.body.should eq "pong"
+    end
+  end
 
-  it "works" do
-    false.should eq(true)
+  describe "GET /version" do
+    it "renders the version" do
+      get "/version"
+      JSON.parse(response.body)["data"]["version"].should eq(Randoms::VERSION)
+    end
+  end
+
+  describe "GET /hex" do
+    it "renders some HEX string" do
+      get "/hex"
+      JSON.parse(response.body)["data"]["random"].to_s.should match(/[0-9a-fA-F]+/)
+    end
+  end
+
+  describe "GET /uuid" do
+    it "renders some UUID string" do
+      get "/uuid"
+      JSON.parse(response.body)["data"]["random"].to_s.should match(/([\w]{8}(-[\w]{4}){3}-[\w]{12})/)
+    end
+  end
+
+  describe "GET /base64" do
+    it "renders some base64 string" do
+      get "/base64"
+      JSON.parse(response.body)["data"]["random"].to_s.should match(/^(?:[A-Za-z0-9+\/]{4})*(?:[A-Za-z0-9+\/]{2}==|[A-Za-z0-9+\/]{3}=|[A-Za-z0-9+\/]{4})$/)
+    end
   end
 end
